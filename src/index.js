d671c2ea66c469793e6790165e65860571819caf
mookar <mookar@Denics-MacBook-Air.local>
2018-09-13 11:24:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let result = {}
+    
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    } else if (currency <= 0) {
+        return result
+    }
+    
+    result["H"] = Math.trunc(currency / 50)
+    result["Q"] = Math.trunc((currency % 50) / 25)
+    result["D"] = Math.trunc(((currency % 50) % 25) / 10)
+    result["N"] = Math.trunc((((currency % 50) % 25) % 10) / 5)
+    result["P"] = Math.trunc(currency % 5)
+
+    for (let item in result) {
+        if (!result[item]){
+            delete result[item]
+        }
+    }
+
+    return result
 }
